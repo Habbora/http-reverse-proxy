f054041b687f516961ff428a0155701393615ed0
Alisson Queiroz de Azevêdo <alisson@habbora.com.br>
2026-02-16 00:32:10 -0300

    chore: update .dockerignore to remove drizzle directory and fix newline

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -2,5 +2,5 @@ import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 import { db } from "../db";
 
 export const runMigrations = async () => {
-    await migrate(db, { migrationsFolder: "drizzle" });
+    await migrate(db, { migrationsFolder: "./drizzle" });
 }
